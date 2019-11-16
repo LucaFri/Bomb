e7cb3097c09642da538d0666610c6b8aae79824b
Luca Frigenti <illuchino@hotmail.it>
2019-11-16 16:59:35 +0100

    Aggiungere i file di progetto.

---
+++ BombCountdown/BombForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace BombCountdown
+{
+    partial class BombForm
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BombForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1902, 1022);
+            this.ControlBox = false;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1918, 1038);
+            this.Name = "BombForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.BombForm_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BombForm_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.BombForm_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.BombForm_KeyUp);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.BombForm_PreviewKeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BombCountdown/BombForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BombCountdown
+{
+    public partial class BombForm : Form
+    {
+        public BombForm()
+        {
+            InitializeComponent();
+            this.Location = Screen.AllScreens[1].WorkingArea.Location;
+        }
+
+        private void BombForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.LWin)
+                e.Handled = true;
+        }
+
+        private void BombForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+
+        private void BombForm_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void BombForm_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+
+        }
+    }
+}
