93d3b61325b3788a4ba1f53b59637dfcd451c91f
Luca Frigenti <illuchino@hotmail.it>
2019-11-23 18:00:22 +0100

    Impostata textBox per inserimento password

--- BombCountdown/BombForm.Designer.cs
+++ BombCountdown/BombForm.Designer.cs
@@ -29,21 +29,21 @@
         private void InitializeComponent()
         {
             this.sevenSegmentArrayCountdown = new DmitryBrant.CustomControls.SevenSegmentArray();
+            this.password = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // sevenSegmentArrayCountdown
             // 
-            this.sevenSegmentArrayCountdown.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.sevenSegmentArrayCountdown.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.sevenSegmentArrayCountdown.ArrayCount = 6;
-            this.sevenSegmentArrayCountdown.ColorBackground = System.Drawing.Color.DarkGray;
+            this.sevenSegmentArrayCountdown.ColorBackground = System.Drawing.Color.Black;
             this.sevenSegmentArrayCountdown.ColorDark = System.Drawing.Color.DimGray;
-            this.sevenSegmentArrayCountdown.ColorLight = System.Drawing.Color.Green;
+            this.sevenSegmentArrayCountdown.ColorLight = System.Drawing.Color.Red;
             this.sevenSegmentArrayCountdown.DecimalShow = true;
-            this.sevenSegmentArrayCountdown.ElementPadding = new System.Windows.Forms.Padding(4);
-            this.sevenSegmentArrayCountdown.ElementWidth = 10;
-            this.sevenSegmentArrayCountdown.ItalicFactor = 0F;
+            this.sevenSegmentArrayCountdown.ElementPadding = new System.Windows.Forms.Padding(12, 5, 28, 5);
+            this.sevenSegmentArrayCountdown.ElementWidth = 11;
+            this.sevenSegmentArrayCountdown.ItalicFactor = 0.01F;
             this.sevenSegmentArrayCountdown.Location = new System.Drawing.Point(0, 0);
             this.sevenSegmentArrayCountdown.Name = "sevenSegmentArrayCountdown";
             this.sevenSegmentArrayCountdown.Size = new System.Drawing.Size(900, 200);
@@ -51,12 +51,27 @@
             this.sevenSegmentArrayCountdown.TabStop = false;
             this.sevenSegmentArrayCountdown.Value = null;
             // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Verdana Pro Cond", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(45, 230);
+            this.password.MaxLength = 4;
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(281, 124);
+            this.password.TabIndex = 1;
+            this.password.TabStop = false;
+            this.password.Text = "0000";
+            this.password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.password.TextChanged += new System.EventHandler(this.password_TextChanged);
+            this.password.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.password_KeyPress);
+            // 
             // BombForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1902, 1022);
             this.ControlBox = false;
+            this.Controls.Add(this.password);
             this.Controls.Add(this.sevenSegmentArrayCountdown);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
@@ -73,12 +88,14 @@
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.BombForm_KeyUp);
             this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.BombForm_PreviewKeyDown);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private DmitryBrant.CustomControls.SevenSegmentArray sevenSegmentArrayCountdown;
+        private System.Windows.Forms.TextBox password;
     }
 }
 
--- BombCountdown/BombForm.cs
+++ BombCountdown/BombForm.cs
@@ -12,26 +12,37 @@ namespace BombCountdown
 {
     public partial class BombForm : Form
     {
+        #region Campi privati
+
+        bool _firstTime;
+
+        #endregion        
 
         #region Costruttori
         public BombForm()
         {
             InitializeComponent();
             this.Location = Screen.AllScreens[1].WorkingArea.Location;
-            Point startLocationCentralControl = GetCentralPoint(sevenSegmentArrayCountdown);
-            sevenSegmentArrayCountdown.Location = startLocationCentralControl;
-        } 
+            Point startLocationCentralSevenSegment = GetCentralPoint(sevenSegmentArrayCountdown);
+            sevenSegmentArrayCountdown.Location = startLocationCentralSevenSegment;
+            sevenSegmentArrayCountdown.Value = "35.56";
+            Point startLocationCentralTextBox = GetCentralPoint(password);
+            password.Location = new Point(startLocationCentralTextBox.X, startLocationCentralTextBox.Y
+                + sevenSegmentArrayCountdown.Height + 30);
+        }
         #endregion
 
         #region Metodi privati
-        private Point GetCentralPoint(UserControl userControl)
+        private Point GetCentralPoint(Control control)
         {
-            return new Point((this.Width / 2) - userControl.Width / 2,
-                (this.Height / 2) - userControl.Height / 2);
-        } 
+            return new Point((this.Width / 2) - control.Width / 2,
+                (this.Height / 2) - control.Height / 2);
+        }
         #endregion
 
         #region Eventi
+
+        #region Eventi Form
         private void BombForm_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyData == Keys.LWin)
@@ -51,7 +62,43 @@ namespace BombCountdown
         private void BombForm_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
         {
 
-        } 
+        }
+
+        #endregion
+
+        #region Eventi TextBox password
+        private void password_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            switch (e.KeyChar)
+            {
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                case '\b':
+                    break;
+                default:
+                    e.Handled = true;
+                    break;
+            }
+        }
+
+        private void password_TextChanged(object sender, EventArgs e)
+        {
+            if (password.Text == "1234")
+            {
+                MessageBox.Show("Bravo hai trovato la password!!!");
+            }
+        }
+
+        #endregion
+
         #endregion
     }
 }
